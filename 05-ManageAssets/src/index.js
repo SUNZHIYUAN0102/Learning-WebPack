6cf0d51cc7ed3a7d039076b46a77bb825f61cb5b
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-05-19 00:43:53 +0700

    Load css

---
+++ 05-ManageAssets/src/index.js
@@ -0,0 +1,30 @@
+import HelloWorld from "./hello-word"
+import imgsrc from './asset/resource/1.png'
+import logosvg from './asset/resource/grooming-1801287.svg'
+import exampleTxt from './asset/resource/example.txt'
+import jpgMap from './asset/resource/Avatar.jpg'
+import './style.css'
+import './style.less'
+
+HelloWorld()
+
+const img = document.createElement('img')
+img.src = imgsrc
+document.body.appendChild(img)
+
+const img2 = document.createElement('img')
+img2.src = logosvg
+img2.style.width = "200px"
+document.body.appendChild(img2)
+
+const txt = document.createElement('div')
+txt.innerText = exampleTxt
+document.body.appendChild(txt)
+
+const img3 = document.createElement('img')
+img3.src = jpgMap
+img3.style.width = "200px"
+document.body.appendChild(img3)
+
+
+document.body.classList.add('hello')
